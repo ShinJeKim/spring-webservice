76e8640b11fe041eca4cd3ea1eb6a946775f1297
ShinJeKim <shinjehere@gmail.com>
2019-02-19 21:49:10 +0900

    프로젝트 생성

---
+++ src/test/java/com/sjkim/webservice/webserviceTests.java
@@ -0,0 +1,16 @@
+package com.sjkim.webservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class webserviceTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
